95f835d0b6eed3bcf81b175d142924e07fe04daa
Gian Paolo Astorino <gianpx@hotmail.com>
2023-07-11 02:05:10 -0400

    2-player playability

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Domino-Redes
 Proyecto Juego de Domino con Comunicación Serial
+
+1. Instalar Python 3
+2. pip install pygame
+3. pip install pyserial
+4. Cuando ejecuten el juego tienen que pasarle por consola el nombre del puerto serial (COM1, COM2 y tal)
+y luego pasarle el número de jugador. 1 para el líder y 2 para el que juega después
+5. Si quieren jugarlo de varios necesitan el Virtual Serial Ports Emulator
--- domino.py
+++ domino.py
@@ -1,5 +1,23 @@
-import pygame
-import random
+import pygame, random, serial, json
+
+#Serial Functions
+def write(header,object):
+    ser.write((header+json.dumps(object)+'\n').encode('utf-8'))
+
+def writeText(text):
+    ser.write((text+'\n').encode('utf-8'))
+
+def read():
+    text=ser.readline()
+    text=text.decode('utf-8')
+    text=text.replace('\n','')
+    return text
+
+def writeAll():
+    write('b',board)
+    write('d',dominoes)
+    write('p',player_hand)
+    write('c',computer_hand)
 
 # initialize Pygame
 pygame.init()
@@ -32,6 +50,56 @@ player_hand = dominoes[:7]
 computer_hand = dominoes[7:14]
 board = [dominoes[14]]
 
+turn = bool
+
+#set up Serial Conection
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.timeout = 0.1
+ser.port = input('Ingrese el nombre del puerto serial: ')
+ser.open()
+playerNumber = input('Ingrese el número del jugador (1/2): ')
+
+#Set up game
+if playerNumber == '1':
+    print('Esperando jugador 2...')
+    ready = False
+    while not ready:
+        writeAll()
+        readyText=read()
+        if readyText == 'ready':
+            ready=True
+            print('Empieza el juego')
+    turn = True
+elif playerNumber == '2':
+    dominoesReady = False
+    boardReady = False
+    player_handReady = False
+    computer_handReady = False
+    ready = False
+    while not ready:
+        text=read()
+        match text[0]:
+            case 'b':
+                text=text.replace('b','')
+                board=json.loads(text)
+                boardReady = True
+            case 'd':
+                text=text.replace('d','')
+                dominoes=json.loads(text)
+                dominoesReady = True
+            case 'p':
+                text=text.replace('p','')
+                computer_hand=json.loads(text)
+                computer_handReady=True
+            case 'c':
+                text=text.replace('c','')
+                player_hand=json.loads(text)
+                player_handReady=True
+        if boardReady and dominoesReady and player_handReady and computer_handReady:
+            ready=True
+            writeText('ready')
+    turn = False
 # set up the game loop
 clock = pygame.time.Clock()
 game_over = False
@@ -42,25 +110,38 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # check if the user clicked on a domino in their hand
-            pos = pygame.mouse.get_pos()
-            for i, domino in enumerate(player_hand):
-                x = 150 + i * 50
-                y = 450
-                if x <= pos[0] <= x + 40 and y <= pos[1] <= y + 60:
-                    # check if the domino can be placed on the board
-                    if domino[0] == board[-1][1]:
-                        board.append(domino)
-                        player_hand.remove(domino)
-                    elif domino[1] == board[-1][1]:
-                        board.append((domino[1], domino[0]))
-                        player_hand.remove(domino)
-                    elif domino[0] == board[0][0]:
-                        board.insert(0, (domino[1], domino[0]))
-                        player_hand.remove(domino)
-                    elif domino[1] == board[0][0]:
-                        board.insert(0, domino)
-                        player_hand.remove(domino)
+            if turn:
+                # check if the user clicked on a domino in their hand
+                pos = pygame.mouse.get_pos()
+                for i, domino in enumerate(player_hand):
+                    x = 150 + i * 50
+                    y = 450
+                    if x <= pos[0] <= x + 40 and y <= pos[1] <= y + 60:
+                        # check if the domino can be placed on the board
+                        if domino[0] == board[-1][1]:
+                            board.append(domino)
+                            player_hand.remove(domino)
+                            writeText('your turn')
+                            writeAll()
+                            turn = False
+                        elif domino[1] == board[-1][1]:
+                            board.append((domino[1], domino[0]))
+                            player_hand.remove(domino)
+                            writeText('your turn')
+                            writeAll()
+                            turn = False
+                        elif domino[0] == board[0][0]:
+                            board.insert(0, (domino[1], domino[0]))
+                            player_hand.remove(domino)
+                            writeText('your turn')
+                            writeAll()
+                            turn = False
+                        elif domino[1] == board[0][0]:
+                            board.insert(0, domino)
+                            player_hand.remove(domino)
+                            writeText('your turn')
+                            writeAll()
+                            turn = False
 
     # draw the board
     screen.fill(GREEN)
@@ -94,6 +175,37 @@ while not game_over:
 
     # update the display
     pygame.display.update()
+
+    if not turn:
+        text=read()
+        if text == 'your turn':
+            dominoesReady = False
+            boardReady = False
+            player_handReady = False
+            computer_handReady = False
+            ready = False
+            while not ready:
+                text=read()
+                match text[0]:
+                    case 'b':
+                        text=text.replace('b','')
+                        board=json.loads(text)
+                        boardReady = True
+                    case 'd':
+                        text=text.replace('d','')
+                        dominoes=json.loads(text)
+                        dominoesReady = True
+                    case 'p':
+                        text=text.replace('p','')
+                        computer_hand=json.loads(text)
+                        computer_handReady=True
+                    case 'c':
+                        text=text.replace('c','')
+                        player_hand=json.loads(text)
+                        player_handReady=True
+                if boardReady and dominoesReady and player_handReady and computer_handReady:
+                    ready = True
+                    turn=True
 
     # limit the frame rate
     clock.tick(60)
